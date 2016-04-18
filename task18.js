a8326438b986fa956c2b2870b261765abc45e79b
HangZhouWan <yydcqc@163.com>
2016-04-18 13:04:41 +0800

    Added files via upload

---
+++ task18.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+window.onload = function(){
+	ChangeList();
+}
+
+var list = [];
+
+
+function ChangeList(){
+	var leftIn = document.getElementById("left-in");
+	var rightIn = document.getElementById("right-in");
+	var leftOut = document.getElementById("left-out");
+	var rightOut = document.getElementById("right-out");
+	leftIn.onclick = Unshift;
+	rightIn.onclick = Push;
+	leftOut.onclick = Shift;
+	rightOut.onclick = Pop;
+}
+function diplayList(){
+	var container = document.getElementById("container");
+	var inner = "";
+	for(i in list){
+		inner += "<span>"+list[i]+"</span>";
+	}
+	container.innerHTML = inner;
+}
+
+function Unshift(){
+	var input = document.getElementById("input").value;
+	if(input!==""){
+	list.unshift(input);
+	}
+	document.getElementById("input").value = "";
+	diplayList()
+}
+function Push(){
+	var input = document.getElementById("input").value;
+	if(input!==""){
+	list.push(input);
+	}
+	document.getElementById("input").value = "";
+	diplayList();
+}
+function Shift(){
+	var input = document.getElementById("input").value;
+	if(list.length!==0){
+	var items = list.shift();
+	alert(items);
+	}
+	diplayList();
+}
+function Pop(){
+	var input = document.getElementById("input").value;
+	if(list.length!==0){
+	var items = list.pop();
+	alert(items);
+	}
+	diplayList();
+}
